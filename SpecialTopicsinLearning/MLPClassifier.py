98d4ac1f2a06aa1a1ff8f272eb3f43499587a816
Leonardo <neves.leonardoc@gmail.com>
2018-12-04 03:06:05 -0200

    MLP updates

--- SpecialTopicsinLearning/MLPClassifier.py
+++ SpecialTopicsinLearning/MLPClassifier.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 
 class MLPClassifier():
-    def __init__(self, mlp_shape=[2,4,1], iteractions=100, alpha=0.2):
+    def __init__(self, mlp_shape=[2,5,3,1], iteractions=30000, alpha=0.1):
         self.iteractions = iteractions
         self.learning_rate = alpha
         self.mlp_shape = mlp_shape
         # Initializing the weights
-        self.__input_weights = np.random.rand(1,self.mlp_shape[0])
-        self.__layer1_weights = np.random.rand(self.mlp_shape[0],self.mlp_shape[1])
+        self.__layer1_weights = np.random.rand(self.mlp_shape[0]+1,self.mlp_shape[1])
         self.__layer2_weights = np.random.rand(self.mlp_shape[1],self.mlp_shape[2])
+        self.__layer3_weights = np.random.rand(self.mlp_shape[2],self.mlp_shape[3])
 
     def help(self):
         print('\n')
@@ -19,15 +19,38 @@ class MLPClassifier():
         print('*'*50)
         print('\n')
 
-    def __sigmoid(x):
+    def __sigmoid(self, x):
         return np.tanh(x)
 
-    def __deltaSigmoid(x):
+    def __deltaSigmoid(self, x):
         return 1.0-x**2
 
     def predict(self, inputs):
-        w1 = np.full((self.mlp_shape[1],1), self.__sigmoid(np.dot(inputs,self.__input_weights.T)))
+        # w1 = np.full((self.mlp_shape[1],1), self.__sigmoid(np.dot(inputs,self.__input_weights.T)))
+        w1 = self.__sigmoid(np.dot(np.append(inputs, [[1]], axis=1), self.__layer1_weights))
+        w2 = self.__sigmoid(np.dot(w1,self.__layer2_weights))
+        w3 = self.__sigmoid(np.dot(w2,self.__layer3_weights))
+        return w1,w2,w3
 
-        w2 = self.__sigmoid(np.dot(w1,self.__layer1_weights.T))
-        w3 = self.__sigmoid(np.dot(w2,self.__layer2_weights.T))
-        return w3
+    def fit(self, traning_data, output_data , learning_rate=0.1):
+        for step in range(self.iteractions):
+            for data_x, data_y in zip(traning_data, output_data):
+
+                data_x = np.append(data_x, [[1]], axis=1)
+
+                out_from_layer1, out_from_layer2, out_from_layer3 = self.predict(data_x)
+
+                error_layer3 = (data_y - out_from_layer3)
+                delta_error3 = error_layer3 * self.__deltaSigmoid(out_from_layer3)
+
+                self.__layer3_weights += learning_rate * delta_error3 * data_y
+
+                error_layer2 = np.dot(delta_error3, self.__layer3_weights.T)
+                delta_error2 = error_layer2 * self.__deltaSigmoid(out_from_layer2)
+
+                self.__layer2_weights += learning_rate * delta_error2 * data_y
+
+                error_layer1 = np.dot(delta_error2, self.__layer2_weights.T)
+                delta_error1 = error_layer1 * self.__deltaSigmoid(out_from_layer1)
+
+                self.__layer1_weights += learning_rate * delta_error1 * data_y
--- class7MLP.py
+++ class7MLP.py
@@ -1,39 +1,26 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
-from SpecialTopicsinLearning.Perceptron import Perceptron
-import numpy as np
+from SpecialTopicsinLearning.MLPClassifier import MLPClassifier
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+import numpy as np
+
 def main():
     '''Instanciando a classe do método'''
-    p = Perceptron(4)
+    net = MLPClassifier()
 
-    iris = datasets.load_iris()
-    df = pd.DataFrame(data=iris.data, columns=['A', 'B', 'C', 'D'])
-    df['target'] = iris.target
+    # iris = datasets.load_iris()
+    # df = pd.DataFrame(data=iris.data, columns=['A', 'B', 'C', 'D'])
+    # df['target'] = iris.target
+    #
+    # train, test = train_test_split(df[df['target'] != 2], test_size=0.2)
+    print("\nSem treino\n")
+    print(net.predict(np.array([[0,1]]))[2])
 
-    train, test = train_test_split(df[df['target'] != 2], test_size=0.2)
+    net.fit(np.array([[0,0], [0,1], [1,0], [1,1]]), np.array([[0, 1, 1, 0]]).T)
+    print("\nCom treino\n")
+    print(net.predict(np.array([[1,1]]))[2])
 
-    # p.fit(X=np.array([[0,0],[0,1],[1,0],[1,1]]), Y=np.array([0,1,1,0]))
-
-    p.fit(X=train.iloc[:,0:4].values, Y=train.iloc[:,4:].values)
-
-    for x in range(len(test)):
-
-        print("\n\t" + "#"*10 + " Predicted " + "#"*9)
-        print("\t" + "#"*30)
-        input = test.iloc[x,0:4].values
-        # input = np.array([[0,x]])
-        print("\tInputs: {}".format(input))
-        print("\tPredicted Class: {}".format(p.predict(input)))
-        print("\tY: {}".format(test.iloc[x,4:].values))
-        # print("\tY: {}".format(x))
-        print("\tWeights: {}".format(p.weights))
-        print("\t" + "#"*30 + "\n")
-
-    # plt.scatter(train.iloc[:,0].values,train.iloc[:,1].values)
-    # plt.show()
 
 if __name__ == '__main__':
     main()
